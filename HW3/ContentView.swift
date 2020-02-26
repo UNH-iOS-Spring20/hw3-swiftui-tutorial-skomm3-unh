fa709db280b05f772df54815ef61de6c16b681f3
Sathvik Chandupatla <schan9@unh.newhaven.edu>
2020-02-26 01:26:40 -0500

    Initial Commit

---
+++ HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3
+//
+//  Created by Shiva Kommareddy on 2/26/20.
+//  Copyright © 2020 Shiva Kommareddy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
